88a82b3216bf051ce6210fb1395fb70059eb46a1
FedericoSpina2 <federico.spina@studenti.unipg.it>
2026-06-08 20:19:53 +0200

    m

---
+++ provaF/server/server.js
@@ -0,0 +1,17 @@
+const zmq= require("zeromq");
+
+async function run(){
+    const sock= new zmq.Pull();
+    await sock.bind("tcp://0.0.0.0:3000");
+    console.log("in ascolot su 0.0.0.0:3000");
+
+    for await (const [msg] of sock){
+        console.log("Ricevuto: ", msg.toString());
+    }
+}
+
+
+run().catch(err =>{
+    console.error(err);
+    process.exit(1);
+})
